a5f1b57e5d7057e2bc9871ab5740417d7b622b64
Dave <dave@laptop-2.local>
2026-06-15 14:55:33 -0400

    Added Alt-Tab fork.

---
+++ Casks/alt-tab.rb
@@ -0,0 +1,40 @@
+cask "alt-tab" do
+  version "11.3.0"
+  sha256 "sha256:1e01d62a9a1a71c086fdeda0194e5cb063a787c50ff1908903d6f9082e55c26f"
+
+  url "https://github.com/dzaikos/alt-tab-macos/releases/download/v#{version}/AltTab-#{version}.zip"
+  name "AltTab"
+  desc "Enable Windows-like alt-tab"
+  homepage "https://github.com/dzaikos/alt-tab-macos"
+
+  auto_updates false
+  depends_on :macos
+
+  app "AltTab.app"
+
+  preflight do
+    system_command "pkill", args: ["AltTab"], must_succeed: false
+    system_command "tccutil", args: ["reset", "Accessibility", "com.lwouis.alt-tab-macos"], must_succeed: false
+    system_command "tccutil", args: ["reset", "ScreenCapture", "com.lwouis.alt-tab-macos"], must_succeed: false
+  end
+
+  postflight do
+    system_command "sudo", args: ["xattr", "-cr", "#{appdir}/AltTab.app"], sudo: true
+    system_command "open", args: ["#{appdir}/AltTab.app"]
+  end
+
+  uninstall quit: "com.lwouis.alt-tab-macos"
+
+  zap trash: [
+    "~/Library/Application Support/com.lwouis.alt-tab-macos",
+    "~/Library/Caches/com.lwouis.alt-tab-macos",
+    "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.lwouis.alt-tab-macos",
+    "~/Library/Cookies/com.lwouis.alt-tab-macos.binarycookies",
+    "~/Library/HTTPStorages/com.lwouis.alt-tab-macos",
+    "~/Library/HTTPStorages/com.lwouis.alt-tab-macos.binarycookies",
+    "~/Library/LaunchAgents/com.lwouis.alt-tab-macos.plist",
+    "~/Library/Preferences/com.lwouis.alt-tab-macos.license.plist",
+    "~/Library/Preferences/com.lwouis.alt-tab-macos.plist",
+    "~/Library/Preferences/com.lwouis.alt-tab-macos.usage.plist",
+  ]
+end
